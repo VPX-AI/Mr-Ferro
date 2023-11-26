8e5f966a745c744e1b5f264dc8bfbfce134f5f74
Mr Ferro <151021441+VPX-AI@users.noreply.github.com>
2023-11-26 15:23:36 +0530

    Added Links , Images File

---
+++ VPX main Chatbot Version {Ferro}/Main_Folder/Links.py
@@ -0,0 +1,16 @@
+import webbrowser
+
+Yt = "https://www.youtube.com"
+
+def Youtube_Link():
+    Yt_url = Yt
+    webbrowser.open(Yt_url)
+def Chat_Gpt_Link():
+    GPT_url= "https://chat.openai.com/"
+    webbrowser.open(GPT_url)
+def Owner_Website():
+    My_url = "https://mr-ferro.blogspot.com/"
+    webbrowser.open(My_url)
+def Source_Code_Link():
+    Code_url = "https://mr-ferro.blogspot.com/2023/11/my-vpx-ai-code.html"
+    webbrowser.open(Code_url)
