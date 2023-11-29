521a283c7c9604b8af984aec1b90218eb1ac7da5
Mr Ferro <151021441+VPX-AI@users.noreply.github.com>
2023-11-29 17:00:41 +0530

    Last Update ==> 29 Nov 2023

--- VPX main Chatbot Version {Ferro}/Main_Folder/Check_Speaker.py
+++ VPX main Chatbot Version {Ferro}/Main_Folder/Check_Speaker.py
@@ -1,13 +1,22 @@
+import banner , Const_Vari
+
 import warnings
 from pydub import AudioSegment
 from pydub.playback import play
 
-warnings.filterwarnings("ignore", message="Couldn't find ffmpeg or avconv")
+print(Const_Vari.Line_Break*2)
+banner.Ferro_Sound_Banner()
+print(Const_Vari.Line_Break*2)
 
 try:
     silent_segment = AudioSegment.silent(duration=100)
     play(silent_segment)
     Speakers_Connected = True
+    print(Const_Vari.Line_Break)
+    print("Speaks are Connected. So There Will be some Sound, If Speakes Are not Connected Then Also its Show Error then Tell This to Head Of Project By going to \"https://linksly.co/7al8xVi\"")
+    print(Const_Vari.Line_Break)
 except Exception as e:
     Speakers_Connected = False
-    print(f"Error: {e}")
+    print(Const_Vari.Line_Break)
+    print("No Speaks Connected. So No Sound, If Speakes Are Connected Then Also its Show Error then Tell This to Head Of Project By going to \"https://linksly.co/7al8xVi\"")
+    print(Const_Vari.Line_Break)
